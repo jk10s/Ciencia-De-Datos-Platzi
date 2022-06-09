ba2a8c6929873009621094b62f8aa9d2f2eac757
jk10s <juanka_267@hotmail.com>
2022-06-09 01:33:57 -0500

    first commit

---
+++ Programacion Orinetada a Ojectos/Account.java
@@ -0,0 +1,13 @@
+public class Account {
+    Integer id;
+    String name;
+    String documento;
+    Integer email;
+    Integer password;
+
+    public Account(String name,String documento) {
+        this.name=name;
+        this.documento=documento;
+        
+    }
+}
