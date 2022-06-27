1c30499b4e435cf5f2ab44d86dba750b84fb17db
jk10s <juanka_267@hotmail.com>
2022-06-27 17:54:33 -0500

    pandas and numpy

---
+++ Curso de Manipulación y Análisis de Datos con Pandas y Python/jk.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import numpy as np
+
+# inicio = int(input('Introduce el año inicial: '))
+# fin = int(input('Introduce el año final: '))
+# ventas = {}
+# for i in range(inicio, fin+1):
+#     ventas[i] = float(input('Introduce las ventas del año ' + str(i) +': '))
+# ventas = pd.Series(ventas)
+# print('Ventas\n', ventas)
+# print('Ventas con descuento\n', ventas*0.9)
+
+tres= np.array([[[1,2,3],[-1,-2,-3]],[[4,5,6],[-4,-5,-6]]])
+print(tres)
