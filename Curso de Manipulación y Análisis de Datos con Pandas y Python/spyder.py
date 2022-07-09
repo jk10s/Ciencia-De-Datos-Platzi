0946acee3c21ab959c33b9acf6d00decbf09f15c
jk10s <juanka_267@hotmail.com>
2022-07-08 22:33:27 -0500

    archivos en spyder

---
+++ Curso de Manipulación y Análisis de Datos con Pandas y Python/spyder.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+import numpy as np
+pob=pd.read_csv('jk10s/poblacion.csv',encoding="latin1")
