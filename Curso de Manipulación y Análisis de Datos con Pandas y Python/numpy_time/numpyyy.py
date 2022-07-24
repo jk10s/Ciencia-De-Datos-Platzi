67ded159c951e66b42d1197eab1e2db72731dfef
jk10s <juanka_267@hotmail.com>
2022-07-23 23:16:46 -0500

    numpyy

---
+++ Curso de Manipulación y Análisis de Datos con Pandas y Python/numpy_time/numpyyy.py
@@ -0,0 +1,17 @@
+import numpy as np
+import time
+import sys
+
+
+
+#Dimensionalidad
+#0D
+escalar_0d = 10
+#1D
+vector_1d = np.array([1,2,3,4,5])
+#2D
+matriz_2d = np.array([[1,2,3],[4,5,6],[7,8,9]])
+#3D
+matriz_3d = np.array([[[1,2,3],[4,5,6],[7,8,9]],[[11,11,13],[14,15,16],[17,18,19]]])
+
+matriz_2d[:,1]
