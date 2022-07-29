19f966992ea11f2298efa32ec0747e70df0771f5
jk10s <juanka_267@hotmail.com>
2022-07-28 22:39:58 -0500

    datetime

---
+++ Curso de Manipulación y Análisis de Datos con Pandas y Python/numpy_time/datetime.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pandas as pd
+import numpy as np
+import datatime
+
+fecha_ejecucion= datatime.datatime.now()
+print(type(fecha_ejecucion))
+print(fecha_ejecucion)
+
+fecha_ejecucion.day
+fecha_ejecucion.year
+fecha_ejecucion.date()
